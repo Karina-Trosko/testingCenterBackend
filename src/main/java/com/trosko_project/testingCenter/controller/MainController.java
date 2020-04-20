b08c228e8356bb95e460a6ab39303ad2f18a8c01
Dell <trosko556@gmail.com>
2020-04-21 00:02:43 +0300

    remove frontend part on a separate project

--- src/main/java/com/trosko_project/testingCenter/controller/MainController.java
+++ src/main/java/com/trosko_project/testingCenter/controller/MainController.java
@@ -1,12 +1,34 @@
 package com.trosko_project.testingCenter.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.*;
+
+@RestController
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("/api/hello")
     public String start() {
-        return "start";
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        JSONArray ja = new JSONArray();
+        ja.put(list);
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+
+        JSONObject employeeObject = new JSONObject();
+        employeeObject.put("employee", employeeDetails);
+        System.out.println(employeeObject);
+        System.out.println(list.toString());
+        return list.toString();
     }
 }
